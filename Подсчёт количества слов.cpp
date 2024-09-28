c02ceaffa6c36807fcb17b983322afadca2d399f
Maksim <maksim.antonov2012@yandex.ru>
2024-09-28 12:33:43 +0300

    3

---
+++ Подсчёт количества слов.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string input;
+  getline(std::cin, input);
+  int i = 0, j = 0, counter = 0;
+  while (j > -1) {
+    j = input.find(' ', i + 1);
+    if (input.substr(i, j-i) != " ")
+      counter++;
+    i = j;
+  }
+  std::cout << counter;
+}
