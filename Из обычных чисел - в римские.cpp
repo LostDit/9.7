c668ee5b42a2d225927c5a4a4a7153035e5bff69
Maksim <maksim.antonov2012@yandex.ru>
2024-11-02 14:08:51 +0300

    5
    Всё ещё думаю как сделать

---
+++ Из обычных чисел - в римские.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::string stroka;
+	
+	std::cout << "Ввод: ";
+	getline(std::cin, stroka);
+
+	
+	//MMMDCCCCLXXXXVIIII - максимальное число
+}
